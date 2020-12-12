8d555e63adfa5f455e654e2064858c17f1e7ecae
James Speirs <james.speirs@gmail.com>
2020-12-12 12:33:51 -0700

    fix output to match docs

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 
 # Output the API Gateway url
-output "api_base_url" {
+output "base_url" {
   value = aws_api_gateway_deployment.deployment ? aws_api_gateway_deployment.deployment.invoke_url : "API not public."
 }

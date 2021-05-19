8bde200b221e0f95d39508b26d49b818be5e87e7
James Speirs <james.speirs@gmail.com>
2021-05-19 15:27:19 -0600

    add access to log to cloudwatch

--- module.tf
+++ module.tf
@@ -94,6 +94,33 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+# Give access to write logs to cloudwatch
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda-logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        Action: [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource: "arn:aws:logs:*:*:*",
+        Effect: "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
 # Handle requests to non-root path. Handles /*
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
